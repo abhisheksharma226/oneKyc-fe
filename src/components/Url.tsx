bf463be69590d0c1fd42513a7aac8a30044ba972
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-03-24 11:58:03 +0530

    url

--- src/components/Url.tsx
+++ src/components/Url.tsx
@@ -1,3 +1,3 @@
 // Desc: URL for the API
-export const BASE_URL = (userId: string) => `https://e3kisse226.us-west-2.awsapprunner.com/api/dashboard?userId=${userId}`;
+export const BASE_URL = (userId: string) => `https://uzmvthtak3.us-west-2.awsapprunner.com/api/dashboard?userId=${userId}`;
 
