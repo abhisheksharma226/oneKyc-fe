6c30a486f12c660c37b8652d9ebba95a7285c203
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-04-04 10:13:09 +0530

    url added

--- src/components/Url.tsx
+++ src/components/Url.tsx
@@ -1,3 +1,3 @@
 // Desc: URL for the API
-export const BASE_URL = (userId: string) => `https://6ndgetwc9p.us-west-2.awsapprunner.com/api/dashboard?userId=${userId}`;
+export const BASE_URL = (userId: string) => `https://bh3ex2p5zu.us-west-2.awsapprunner.com/api/dashboard?userId=${userId}`;
 
